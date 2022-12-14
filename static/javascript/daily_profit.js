62bdde1e513573d8451255aa67197193ad97a45b
Rakxit121 <rakxit121@outlook.com>
2022-12-14 11:23:57 +0545

    pie chart

--- static/javascript/daily_profit.js
+++ static/javascript/daily_profit.js
@@ -31,33 +31,27 @@ new Chart(chart, {
 
 
 
-const config = {
-    type: 'bar',
-    data,
-    options: {
-      indexAxis: 'y',
-    }
-  };
 
 
-const day = Utils.days({count: 7});
-const data = {
-  labels: day,
-  datasets: [{
-    axis: 'y',
-    label: 'Daily profit',
-    data: [1030, 2400, 5300, 2400, 5040, 6500, 7600],
-    fill: false,
-    backgroundColor: [
-      'rgba(255, 99, 132, 0.2)',
+var XVal = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
+var YVal = [1030, 2400, 5300, 2400, 5040, 6500, 7600];
+var barcolor = ['rgba(255, 99, 132, 0.2)',
       'rgba(255, 159, 64, 0.2)',
       'rgba(255, 205, 86, 0.2)',
       'rgba(75, 192, 192, 0.2)',
       'rgba(54, 162, 235, 0.2)',
       'rgba(153, 102, 255, 0.2)',
-      'rgba(201, 203, 207, 0.2)'
-    ],
-    borderColor: [
+      'rgba(201, 203, 207, 0.2)'];
+new Chart("pie-graph", {
+  type:"doughnut",
+  data: {
+    labels: XVal,
+    datasets: [{
+      axis: 'y',
+      label: "Daily profit",
+      data: YVal,
+      backgroundColor: barcolor,
+          borderColor: [
       'rgb(255, 99, 132)',
       'rgb(255, 159, 64)',
       'rgb(255, 205, 86)',
@@ -66,6 +60,15 @@ const data = {
       'rgb(153, 102, 255)',
       'rgb(201, 203, 207)'
     ],
-    borderWidth: 1
-  }]
-};
+      }]
+      },
+      options: {
+        legend: {display: false},
+        title: {
+          display: true,
+          text: 'Daily profit'
+          }
+        }
+    });
+
+
