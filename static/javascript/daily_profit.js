35143fae926c48c9a46d56823a42c6d524e22aad
Rakxit121 <rakxit121@outlook.com>
2022-12-14 14:28:11 +0545

    bar graph of admin dashboard

--- static/javascript/daily_profit.js
+++ static/javascript/daily_profit.js
@@ -72,3 +72,45 @@ new Chart("pie-graph", {
     });
 
 
+
+    
+var XVal = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
+var YVal = [1030, 2400, 5300, 2400, 5040, 6500, 7600];
+var barcolor = ['rgba(255, 99, 132, 0.2)',
+      'rgba(255, 159, 64, 0.2)',
+      'rgba(255, 205, 86, 0.2)',
+      'rgba(75, 192, 192, 0.2)',
+      'rgba(54, 162, 235, 0.2)',
+      'rgba(153, 102, 255, 0.2)',
+      'rgba(201, 203, 207, 0.2)'];
+new Chart("bar-graph", {
+  type:"bar",
+  data: {
+    labels: XVal,
+    datasets: [{
+      // axis: 'y',
+      label: "Daily profit",
+      data: YVal,
+      backgroundColor: barcolor,
+          borderColor: [
+      'rgb(255, 99, 132)',
+      'rgb(255, 159, 64)',
+      'rgb(255, 205, 86)',
+      'rgb(75, 192, 192)',
+      'rgb(54, 162, 235)',
+      'rgb(153, 102, 255)',
+      'rgb(201, 203, 207)'
+    ],
+      }]
+      },
+      options: {
+        legend: {display: false},
+        title: {
+          display: true,
+          text: 'Daily profit'
+          }
+        }
+    });
+
+
+
