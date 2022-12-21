2e8b73223c09f2e5d0beff3b0c8ea82c37f77315
Abhishek Karki <abhishek@Abhisheks-MacBook-Pro.local>
2022-12-21 13:48:31 +0545

    UI

---
+++ static/javascript/nearby_places.js
@@ -0,0 +1,6 @@
+function kathmandu_map() {
+  window.open("https://www.google.com/maps/place/Kathmandu+44600/@27.7090319,85.2911132,13z/data=!3m1!4b1!4m5!3m4!1s0x39eb198a307baabf:0xb5137c1bf18db1ea!8m2!3d27.7172453!4d85.3239605");
+}
+function pokhara_map(){
+  window.open("https://www.google.com/maps/place/Pokhara/@28.2148106,83.962426,12.36z/data=!4m5!3m4!1s0x3995937bbf0376ff:0xf6cf823b25802164!8m2!3d28.2095831!4d83.9855674")
+}
