8712405e6bab9c04efd192b844e5dc68b7a6bdb5
abishek07bimali <abi0shekbimali@gmail.com>
2022-12-21 19:56:20 +0545

    updates on Qr scam

--- static/javascript/qrscanner.js
+++ static/javascript/qrscanner.js
@@ -1,5 +1,5 @@
 var qrcode = new QRCode(document.querySelector(".qrcode"));
-qrcode.makeCode("luxury restaurent");
+qrcode.makeCode("luxury hotel");
 function generateQr() {
   qrcode.makeCode(document.querySelector("input").value);
 }
