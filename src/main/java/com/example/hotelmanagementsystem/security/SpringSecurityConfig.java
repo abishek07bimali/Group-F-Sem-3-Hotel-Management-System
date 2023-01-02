cd960d6c04783adbb4e7c9a059ca548628fe5f6a
abishek07bimali <abi0shekbimali@gmail.com>
2023-01-02 09:01:17 +0545

    Booking error Fixed

--- src/main/java/com/example/hotelmanagementsystem/security/SpringSecurityConfig.java
+++ src/main/java/com/example/hotelmanagementsystem/security/SpringSecurityConfig.java
@@ -16,7 +16,7 @@ public class SpringSecurityConfig {
     protected SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws  Exception{
         httpSecurity.csrf().disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/homepage","/user/**","/admin/**","/css/**","/js/**","/png/**","/jpg/**")
+                .requestMatchers("/homepage","/user/**","/admin/**","/css/**","/js/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
