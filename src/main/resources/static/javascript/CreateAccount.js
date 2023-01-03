329dd738a8a348cf1dd17c97b40b2e76f634c8c3
Bhawana shahi <shahivawana61@gmail.com>
2023-01-03 16:31:41 +0545

    js

---
+++ src/main/resources/static/javascript/CreateAccount.js
@@ -0,0 +1,29 @@
+// function validate()
+
+// {
+
+// var username=document.getElementById("user").value;
+
+// var password=document.getElementById("pws").value;
+
+//     if(username=="admin" && password=="admin" ){
+
+//       window.location.assign("homepage.html");
+
+//         alert("login succesfull");
+
+     
+
+//     }
+
+//     else{
+
+//         alert("Incorrect UserID or Password");
+
+//     }
+
+// }
+
+var attempt = 3; // Variable to count number of attempts.
+
+// Below function Executes on click of login button.
