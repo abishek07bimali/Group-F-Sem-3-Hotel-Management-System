8db96795087b60474a605af086b2727b3f137531
Amul bashyal <amulbashyal6@gmail.com>
2023-01-08 10:22:25 +0545

    UI changes

--- src/main/java/com/example/hotelmanagementsystem/Controller/UserController.java
+++ src/main/java/com/example/hotelmanagementsystem/Controller/UserController.java
@@ -3,18 +3,13 @@ package com.example.hotelmanagementsystem.Controller;
 import com.example.hotelmanagementsystem.Services.UserService;
 import com.example.hotelmanagementsystem.UserPojo.BookingPojo;
 import com.example.hotelmanagementsystem.UserPojo.UserPojo;
-import com.example.hotelmanagementsystem.entity.User;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-
-import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -40,7 +35,7 @@ public class UserController {
     @GetMapping("/booking")
     public String BookHotel(Model model) {
         model.addAttribute("booking", new BookingPojo());
-        return "booking";
+        return "booking1";
     }
 
 
