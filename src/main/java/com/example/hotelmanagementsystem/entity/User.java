67ff62c85999e8f680c225deed38e5cc178d8749
abhishekkarky <karkyabhishek@gmail.com>
2023-01-10 10:37:59 +0545

    Changes to entity

--- src/main/java/com/example/hotelmanagementsystem/entity/User.java
+++ src/main/java/com/example/hotelmanagementsystem/entity/User.java
@@ -6,6 +6,10 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 
 
 @Getter
@@ -16,7 +20,7 @@ import lombok.Setter;
 @Table(name="users", uniqueConstraints = {
         @UniqueConstraint(name = "hms_user_email", columnNames = "email")
 })
-public class User {
+public class User implements UserDetails {
     @Id
     @SequenceGenerator(name = "hms_user_seq_gen", sequenceName = "hms_user_id_seq", allocationSize = 1)
     @GeneratedValue(generator = "hms_user_seq_gen", strategy = GenerationType.SEQUENCE)
@@ -33,6 +37,36 @@ public class User {
 
     @Column(name = "password",nullable = false)
     private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 
 
 
