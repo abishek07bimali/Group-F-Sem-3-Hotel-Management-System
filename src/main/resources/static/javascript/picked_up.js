c82d8e4fd6be22149e4aa6ca1b7e3244835c31a1
abhishekkarky <karkyabhishek@gmail.com>
2023-01-17 20:20:08 +0545

    Modified JS

--- src/main/resources/static/javascript/picked_up.js
+++ src/main/resources/static/javascript/picked_up.js
@@ -39,3 +39,11 @@ autocomplete.addListener('place_changed', function() {
   });
 });
 
+function myMap() {
+  var mapProp= {
+    center:new google.maps.LatLng(51.508742,-0.120850),
+    zoom:5,
+  };
+  var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+}
+
