906a10c792db7b0eb882a5fedba03cb07b31e5cc
abhishekkarky <karkyabhishek@gmail.com>
2023-01-17 17:15:43 +0545

    simple form js for pickup request added.

---
+++ src/main/resources/static/javascript/picked_up.js
@@ -0,0 +1,41 @@
+var map;
+var input = document.getElementById('locationInput');
+
+map = new google.maps.Map(document.getElementById('map'), {
+  center: {lat: -33.8688, lng: 151.2195},
+  zoom: 13
+});
+
+var autocomplete = new google.maps.places.Autocomplete(input);
+autocomplete.bindTo('bounds', map);
+
+autocomplete.addListener('place_changed', function() {
+    var place = autocomplete.getPlace();
+    console.log(place);
+});
+
+var input = document.getElementById('location-input');
+var autocomplete = new google.maps.places.Autocomplete(input);
+
+autocomplete.addListener('place_changed', function() {
+  var place = autocomplete.getPlace();
+  if (!place.geometry) {
+    // User entered the name of a Place that was not suggested and
+    // pressed the Enter key, or the Place Details request failed.
+    window.alert("No details available for input: '" + place.name + "'");
+    return;
+  }
+
+  // If the place has a geometry, then present it on a map.
+  if (place.geometry.viewport) {
+    map.fitBounds(place.geometry.viewport);
+  } else {
+    map.setCenter(place.geometry.location);
+    map.setZoom(17);  // Why 17? Because it looks good.
+  }
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+  });
+});
+
