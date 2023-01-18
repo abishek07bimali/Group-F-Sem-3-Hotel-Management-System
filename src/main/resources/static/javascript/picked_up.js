b327291eb02ef8dd6da9ff6bdaf6e1b6c3d1bbb8
abhishekkarky <karkyabhishek@gmail.com>
2023-01-18 20:48:57 +0545

    Edited js code for location

--- src/main/resources/static/javascript/picked_up.js
+++ src/main/resources/static/javascript/picked_up.js
@@ -1,49 +1,32 @@
-var map;
-var input = document.getElementById('locationInput');
-
-map = new google.maps.Map(document.getElementById('map'), {
-  center: {lat: -33.8688, lng: 151.2195},
-  zoom: 13
-});
-
-var autocomplete = new google.maps.places.Autocomplete(input);
-autocomplete.bindTo('bounds', map);
-
-autocomplete.addListener('place_changed', function() {
-    var place = autocomplete.getPlace();
-    console.log(place);
-});
-
-var input = document.getElementById('location-input');
-var autocomplete = new google.maps.places.Autocomplete(input);
-
-autocomplete.addListener('place_changed', function() {
-  var place = autocomplete.getPlace();
-  if (!place.geometry) {
-    // User entered the name of a Place that was not suggested and
-    // pressed the Enter key, or the Place Details request failed.
-    window.alert("No details available for input: '" + place.name + "'");
-    return;
+const button = document.querySelector("button");
+button.addEventListener("click", ()=>{
+  if(navigator.geolocation){
+    button.innerText = "Allow to detect location";
+    navigator.geolocation.getCurrentPosition(onSuccess, onError);
+  }else{
+    button.innerText = "Your browser not support";
   }
-
-  // If the place has a geometry, then present it on a map.
-  if (place.geometry.viewport) {
-    map.fitBounds(place.geometry.viewport);
-  } else {
-    map.setCenter(place.geometry.location);
-    map.setZoom(17);  // Why 17? Because it looks good.
-  }
-  var marker = new google.maps.Marker({
-    map: map,
-    position: place.geometry.location
+});
+function onSuccess(position){
+  button.innerText = "Detecting your location...";
+  let {latitude, longitude} = position.coords;
+  fetch(`https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=YOUR_API_KEY`)
+      .then(response => response.json()).then(response =>{
+    let allDetails = response.results[0].components;
+    console.table(allDetails);
+    let {county, postcode, country} = allDetails;
+    button.innerText = `${county} ${postcode}, ${country}`;
+  }).catch(()=>{
+    button.innerText = "Something went wrong";
   });
-});
-
-function myMap() {
-  var mapProp= {
-    center:new google.maps.LatLng(51.508742,-0.120850),
-    zoom:5,
-  };
-  var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
 }
-
+function onError(error){
+  if(error.code == 1){
+    button.innerText = "You denied the request";
+  }else if(error.code == 2){
+    button.innerText = "Location is unavailable";
+  }else{
+    button.innerText = "Something went wrong";
+  }
+  button.setAttribute("disabled", "true");
+}
