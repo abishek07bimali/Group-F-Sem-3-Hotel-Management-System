58a4b837b5c2b462db4d57b5bf54a128bbf9774d
Bhawana shahi <shahivawana61@gmail.com>
2023-01-21 06:24:08 +0545

    fixed all error

--- src/main/java/com/example/hotelmanagementsystem/Services/Impl/UserServiceImpl.java
+++ src/main/java/com/example/hotelmanagementsystem/Services/Impl/UserServiceImpl.java
@@ -99,5 +99,5 @@ public class UserServiceImpl implements UserService {
 
 
 
-}
+
 
--- src/main/java/com/example/hotelmanagementsystem/UserPojo/LaundaryPojo.java
+++ src/main/java/com/example/hotelmanagementsystem/UserPojo/LaundaryPojo.java
@@ -1,6 +1,15 @@
 package com.example.hotelmanagementsystem.UserPojo;
-
 import com.example.hotelmanagementsystem.entity.Laundary;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 
 public class LaundaryPojo {
 
@@ -18,6 +27,7 @@ public class LaundaryPojo {
         this.print=laundary.getPrint();
 
     }
+
 
 }
 
