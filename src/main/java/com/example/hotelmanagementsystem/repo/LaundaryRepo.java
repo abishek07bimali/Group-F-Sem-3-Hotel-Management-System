5630ec7aa807e210ec73b4509b81d2a81c4270dc
Bhawana shahi <shahivawana61@gmail.com>
2023-01-21 05:48:28 +0545

    fixed repo error

---
+++ src/main/java/com/example/hotelmanagementsystem/repo/LaundaryRepo.java
@@ -0,0 +1,7 @@
+package com.example.hotelmanagementsystem.repo;
+
+import com.example.hotelmanagementsystem.entity.Laundary;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LaundaryRepo extends JpaRepository<Laundary, Integer> {
+}
